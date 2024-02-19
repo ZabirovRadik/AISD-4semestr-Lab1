c32c8458689de6c936a6e0a16e3075aeb2f0ee9c
Radik Zabirov <zabirovradik070@gmail.com>
2024-02-19 15:22:54 +0400

    Some changes

--- tests/Search_tree_tests.cc
+++ tests/Search_tree_tests.cc
@@ -47,292 +47,6 @@ TEST(Search_tree, Methods3) {
 	EXPECT_FALSE(Tree.contains(tmp));
 }
 
-//TEST(Search_tree, Experement1Set) {
-//	double sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		Set<size_t> Tree;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 1000; ++j)
-//			Tree.insert(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's the average filling time is 1000 numbers:" << sum / 100 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		Set<size_t> Tree;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 10000; ++j)
-//			Tree.insert(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's the average filling time is 10000 numbers:" << sum / 100 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		Set<size_t> Tree;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 100000; ++j)
-//			Tree.insert(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's the average filling time is 100000 numbers:" << sum / 100 << endl;
-//}
-//
-//TEST(Search_tree, Experement1Vector) {
-//	double sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		vector<size_t> vec;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 1000; ++j)
-//			vec.push_back(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's the average filling time is 1000 numbers:" << sum / 100 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		vector<size_t> vec;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 10000; ++j)
-//			vec.push_back(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's the average filling time is 10000 numbers:" << sum / 100 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 100; ++i) {
-//		vector<size_t> vec;
-//		clock_t t = clock();
-//		for (size_t j = 0; j < 100000; ++j)
-//			vec.push_back(lcg());
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's the average filling time is 100000 numbers:" << sum / 100 << endl;
-//}
-//
-//
-//TEST(Search_tree, Experement2Set) {
-//	size_t rand_i, rand_elem;
-//	
-//	double sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<size_t> Tree;
-//		rand_i = lcg() % 1000;
-//		for (size_t j = 0; j < 1000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				Tree.insert(rand_elem);
-//			}
-//			else
-//				Tree.insert(lcg());
-//		}
-//		clock_t t = clock();
-//		Tree.contains(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's time to search for an element (1000):" << sum / 1000 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<size_t> Tree;
-//		rand_i = lcg() % 10000;
-//		for (size_t j = 0; j < 10000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				Tree.insert(rand_elem);
-//			}
-//			else
-//				Tree.insert(lcg());
-//		}
-//		clock_t t = clock();
-//		Tree.contains(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's time to search for an element (10000):" << sum / 1000 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<size_t> Tree;
-//		rand_i = lcg() % 100000;
-//		for (size_t j = 0; j < 100000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				Tree.insert(rand_elem);
-//			}
-//			else
-//				Tree.insert(lcg());
-//		}
-//		clock_t t = clock();
-//		Tree.contains(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's time to search for an element (100000):" << sum / 1000 << endl;
-//}
-//
-//TEST(Search_tree, Experement2Vector) {
-//	size_t rand_i, rand_elem;
-//	double sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<size_t> vec;
-//		rand_i = lcg() % 1000;
-//		for (size_t j = 0; j < 1000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				vec.push_back(rand_elem);
-//			}
-//			else
-//				vec.push_back(lcg());
-//		}
-//		clock_t t = clock();
-//		find(vec.begin(), vec.end(), rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's time to search for an element (1000):" << sum / 1000 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<size_t> vec;
-//		rand_i = lcg() % 10000;
-//		for (size_t j = 0; j < 10000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				vec.push_back(rand_elem);
-//			}
-//			else
-//				vec.push_back(lcg());
-//		}
-//		clock_t t = clock();
-//		find(vec.begin(), vec.end(), rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's time to search for an element (100000):" << sum / 1000 << endl;
-//
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<size_t> vec;
-//		rand_i = lcg() % 100000;
-//		for (size_t j = 0; j < 100000; ++j) {
-//			if (j == rand_i) {
-//				rand_elem = lcg();
-//				vec.push_back(rand_elem);
-//			}
-//			else
-//				vec.push_back(lcg());
-//		}
-//		clock_t t = clock();
-//		find(vec.begin(), vec.end(), rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's time to search for an element (100000):" << sum / 1000 << endl;
-//}
-//
-//TEST(Search_tree, Experement3Set) {
-//	size_t rand_i, rand_elem;
-//	double sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<int> Tree;
-//		rand_i = lcg() % 1000;
-//		for (size_t j = 0; j < 1000; ++j) {
-//			Tree.insert(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		Tree.insert(rand_elem);
-//		Tree.erase(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Set's time to insert and delete an element:" << endl;
-//	cout << "for 1000 elements:" << sum / 1000 << endl;
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<int> Tree;
-//		rand_i = lcg() % 10000;
-//		for (size_t j = 0; j < 10000; ++j) {
-//			Tree.insert(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		Tree.insert(rand_elem);
-//		Tree.erase(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "for 10000 elements:" << sum / 10000 << endl;
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		Set<int> Tree;
-//		rand_i = lcg() % 100000;
-//		for (size_t j = 0; j < 100000; ++j) {
-//			Tree.insert(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		Tree.insert(rand_elem);
-//		Tree.erase(rand_elem);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "for 100000 elements:" << sum / 100000 << endl;
-//}
-//
-//TEST(Search_tree, Experement3Vector) {
-//	size_t rand_i, rand_elem;
-//	double sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<int> vec;
-//		rand_i = lcg() % 1000;
-//		for (size_t j = 0; j < 1000; ++j) {
-//			vec.push_back(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		vec.insert(vec.begin() + rand_i, rand_elem);
-//		vec.erase(vec.begin() + rand_i);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "Vector's time to insert and delete an element:" << endl;
-//	cout << "for 1000 elements:" << sum / 1000 << endl;
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<int> vec;
-//		rand_i = lcg() % 10000;
-//		for (size_t j = 0; j < 10000; ++j) {
-//			vec.push_back(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		vec.insert(vec.begin() + rand_i, rand_elem);
-//		vec.erase(vec.begin() + rand_i);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "for 10000 elements:" << sum / 10000 << endl;
-//
-//	sum = 0;
-//	for (size_t i = 0; i < 1000; ++i) {
-//		vector<int> vec;
-//		rand_i = lcg() % 100000;
-//		for (size_t j = 0; j < 100000; ++j) {
-//			vec.push_back(lcg());
-//		}
-//		rand_elem = lcg();
-//		clock_t t = clock();
-//		vec.insert(vec.begin() + rand_i, rand_elem);
-//		vec.erase(vec.begin() + rand_i);
-//		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
-//	}
-//	cout << "for 100000 elements:" << sum / 100000 << endl;
-//}
-
 TEST(Search_tree, TaskVariant4) {
 	vector<int> vec;
 	//for (size_t i = 0; i < 90; ++i)
@@ -347,3 +61,290 @@ TEST(Search_tree, TaskVariant4) {
 	for (int i = 0; i < size; ++i)
 		cout << vec[i] << ", ";
 }
+
+TEST(Search_tree, Experement1Set) {
+	double sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		Set<size_t> Tree;
+		clock_t t = clock();
+		for (size_t j = 0; j < 1000; ++j)
+			Tree.insert(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's the average filling time is 1000 numbers:" << sum / 100 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		Set<size_t> Tree;
+		clock_t t = clock();
+		for (size_t j = 0; j < 10000; ++j)
+			Tree.insert(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's the average filling time is 10000 numbers:" << sum / 100 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		Set<size_t> Tree;
+		clock_t t = clock();
+		for (size_t j = 0; j < 100000; ++j)
+			Tree.insert(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's the average filling time is 100000 numbers:" << sum / 100 << endl;
+}
+
+TEST(Search_tree, Experement1Vector) {
+	double sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<size_t> vec;
+		clock_t t = clock();
+		for (size_t j = 0; j < 1000; ++j)
+			vec.push_back(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's the average filling time is 1000 numbers:" << sum / 100 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<size_t> vec;
+		clock_t t = clock();
+		for (size_t j = 0; j < 10000; ++j)
+			vec.push_back(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's the average filling time is 10000 numbers:" << sum / 100 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 100; ++i) {
+		vector<size_t> vec;
+		clock_t t = clock();
+		for (size_t j = 0; j < 100000; ++j)
+			vec.push_back(lcg());
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's the average filling time is 100000 numbers:" << sum / 100 << endl;
+}
+
+
+TEST(Search_tree, Experement2Set) {
+	size_t rand_i, rand_elem;
+	
+	double sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<size_t> Tree;
+		rand_i = lcg() % 1000;
+		for (size_t j = 0; j < 1000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				Tree.insert(rand_elem);
+			}
+			else
+				Tree.insert(lcg());
+		}
+		clock_t t = clock();
+		Tree.contains(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's time to search for an element (1000):" << sum / 1000 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<size_t> Tree;
+		rand_i = lcg() % 10000;
+		for (size_t j = 0; j < 10000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				Tree.insert(rand_elem);
+			}
+			else
+				Tree.insert(lcg());
+		}
+		clock_t t = clock();
+		Tree.contains(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's time to search for an element (10000):" << sum / 1000 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<size_t> Tree;
+		rand_i = lcg() % 100000;
+		for (size_t j = 0; j < 100000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				Tree.insert(rand_elem);
+			}
+			else
+				Tree.insert(lcg());
+		}
+		clock_t t = clock();
+		Tree.contains(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's time to search for an element (100000):" << sum / 1000 << endl;
+}
+
+TEST(Search_tree, Experement2Vector) {
+	size_t rand_i, rand_elem;
+	double sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<size_t> vec;
+		rand_i = lcg() % 1000;
+		for (size_t j = 0; j < 1000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				vec.push_back(rand_elem);
+			}
+			else
+				vec.push_back(lcg());
+		}
+		clock_t t = clock();
+		find(vec.begin(), vec.end(), rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's time to search for an element (1000):" << sum / 1000 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<size_t> vec;
+		rand_i = lcg() % 10000;
+		for (size_t j = 0; j < 10000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				vec.push_back(rand_elem);
+			}
+			else
+				vec.push_back(lcg());
+		}
+		clock_t t = clock();
+		find(vec.begin(), vec.end(), rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's time to search for an element (100000):" << sum / 1000 << endl;
+
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<size_t> vec;
+		rand_i = lcg() % 100000;
+		for (size_t j = 0; j < 100000; ++j) {
+			if (j == rand_i) {
+				rand_elem = lcg();
+				vec.push_back(rand_elem);
+			}
+			else
+				vec.push_back(lcg());
+		}
+		clock_t t = clock();
+		find(vec.begin(), vec.end(), rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's time to search for an element (100000):" << sum / 1000 << endl;
+}
+
+TEST(Search_tree, Experement3Set) {
+	size_t rand_i, rand_elem;
+	double sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<int> Tree;
+		rand_i = lcg() % 1000;
+		for (size_t j = 0; j < 1000; ++j) {
+			Tree.insert(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		Tree.insert(rand_elem);
+		Tree.erase(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Set's time to insert and delete an element:" << endl;
+	cout << "for 1000 elements:" << sum / 1000 << endl;
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<int> Tree;
+		rand_i = lcg() % 10000;
+		for (size_t j = 0; j < 10000; ++j) {
+			Tree.insert(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		Tree.insert(rand_elem);
+		Tree.erase(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "for 10000 elements:" << sum / 10000 << endl;
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		Set<int> Tree;
+		rand_i = lcg() % 100000;
+		for (size_t j = 0; j < 100000; ++j) {
+			Tree.insert(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		Tree.insert(rand_elem);
+		Tree.erase(rand_elem);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "for 100000 elements:" << sum / 100000 << endl;
+}
+
+TEST(Search_tree, Experement3Vector) {
+	size_t rand_i, rand_elem;
+	double sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<int> vec;
+		rand_i = lcg() % 1000;
+		for (size_t j = 0; j < 1000; ++j) {
+			vec.push_back(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		vec.insert(vec.begin() + rand_i, rand_elem);
+		vec.erase(vec.begin() + rand_i);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "Vector's time to insert and delete an element:" << endl;
+	cout << "for 1000 elements:" << sum / 1000 << endl;
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<int> vec;
+		rand_i = lcg() % 10000;
+		for (size_t j = 0; j < 10000; ++j) {
+			vec.push_back(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		vec.insert(vec.begin() + rand_i, rand_elem);
+		vec.erase(vec.begin() + rand_i);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "for 10000 elements:" << sum / 10000 << endl;
+
+	sum = 0;
+	for (size_t i = 0; i < 1000; ++i) {
+		vector<int> vec;
+		rand_i = lcg() % 100000;
+		for (size_t j = 0; j < 100000; ++j) {
+			vec.push_back(lcg());
+		}
+		rand_elem = lcg();
+		clock_t t = clock();
+		vec.insert(vec.begin() + rand_i, rand_elem);
+		vec.erase(vec.begin() + rand_i);
+		sum += (float)(clock() - t) / CLOCKS_PER_SEC;
+	}
+	cout << "for 100000 elements:" << sum / 100000 << endl;
+}
+
